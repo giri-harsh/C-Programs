271f2f2a1d15d98562e5bbe61642c62a996b169f
Harsh Giri <2006.harshgiri@gmail.com>
2024-09-02 12:16:24 +0530

    Update ADDING A NEW PROGRAM

---
+++ Odd or even.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+
+
+int main()
+{
+    int n;
+    printf("\n ENTER A NUM");
+    scanf("%d",&n);
+    if(n%2==0)
+    printf("\n NUM IS EVEN")
+    else 
+    printf("\n NUM IS ODD");
+    getch();
+    
+}
