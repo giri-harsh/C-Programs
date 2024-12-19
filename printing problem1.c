234dba47e07a9ad636e928ace7bb1522822c20f3
Harsh Giri <2006.harshgiri@gmail.com>
2024-12-19 17:56:48 +0530

    Add files via upload

---
+++ printing problem1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+//fucntion to print hello and second to print good bye
+
+void printHello(){
+    printf("Hello\n");
+}
+void printbye(){
+    printf("Good bye\n");
+}
+int main(){
+    printHello();
+    printbye();
+    return 0;
+}
