7968431225fba1371bc582ba642a938ced80390f
Harsh Giri <2006.harshgiri@gmail.com>
2025-01-06 21:01:03 +0530

    Add files via upload

---
+++ Remove a file.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    FILE *ptr;
+    
+    remove("C:\\Users\\Harsh Giri\\OneDrive\\Documents\\C-work\\C-Programs\\File Handling\\Output  File\\Printing1.txt");
+    
+}
