ffab6facc60048aead2d1cf453ecf0aaa752431e
Harsh Giri <2006.harshgiri@gmail.com>
2025-02-27 21:00:49 +0530

    Add files via upload

---
+++ Pointers/calloc.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    int n;
+
+    printf("ENter size of array\n");
+    scanf("%d",&n);
+    int *ptr;
+    ptr = (int*)malloc(n*sizeof(int));
+    //input of array
+    for (int i=0;i<n;i++){
+        scanf("%d",&ptr[i]);
+    }
+
+
+    //output printing
+    for (int i=0;i<n;i++)
+    printf("Elemts are %d \n",ptr[i]);  
+
+    
+}
